9ce4e4cff5889ea27d9404db6b6ef48ae5cde33a
Preksler <preksler@gmail.com>
2022-05-26 02:20:41 +0300

    load images and load-more functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
 import './css/styles.css';
-import axios from 'axios';
-// const debounce = require('lodash.debounce');
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import ImageApiService from './js/fetchImages';
+import ImagesApiService from './js/fetchImages';
+import imagesMarkup from './js/imagesMarkup';
 
+const refs = {
+    form: document.querySelector('#search-form'),
+    galleryList: document.querySelector('.gallery'),
+    loadMoreBtn: document.querySelector('.load-more'),
+};
+
+const imagesApiService = new ImagesApiService();
+
+refs.form.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onSearch(e) {
+    e.preventDefault();
+
+    imagesApiService.query = e.currentTarget.elements.searchQuery.value;
+    imagesApiService.resetPage();
+    imagesApiService.fetchImages()
+        .then(({ data }) => {
+            clearImagesContainer();
+            appendImagesMurkup(data.hits)
+        })
+        .catch(err => {console.log(err);});
+}
+
+function appendImagesMurkup(images) {
+    refs.galleryList.insertAdjacentHTML('beforeend', imagesMarkup(images));
+}
+
+function onLoadMore() {
+    imagesApiService.fetchImages()
+        .then(({ data }) => { appendImagesMurkup(data.hits) });
+}
+
+function clearImagesContainer() {
+    refs.galleryList.innerHTML = '';
+}
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,21 +1,41 @@
-// export default class ImageApiService {
-//     constructor() {
-//         this.searchQuery = '';
-//      }
+import axios from 'axios';
 
-//  fetchImages() {
-//      return fetch(`https://restcountries.com/v2/name/${this.searchQuery}?fields=name,capital,population,flags,languages`)
-//          .then(response => {
-//              if (!response.ok) {
-//                  throw new Error(response.status);
-//              }
-//              return response.json();
-//          });
-//     }
-//     get query() {
-//         return this.searchQuery;
-//     }
-//     set query(newQuery) {
-//         this.searchQuery = newQuery;
-//     }
-// }
+export default class ImagesApiService {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    }
+
+    async fetchImages() {
+        // const params = {
+        //     key: '27643895-f6cbf0652fd699c6e124c20c0',
+        //     q: this.searchQuery,
+        //     page: this.page,
+        //     image_type: 'photo',
+        //     orientation: 'horizontal',
+        //     per_page: 40,
+        //     safesearch: true,
+        // };
+        // const response = await axios.get(`https://pixabay.com/api/`, params);
+
+        const response = await axios.get(`https://pixabay.com/api/?key=27643895-f6cbf0652fd699c6e124c20c0&q=${this.searchQuery}&page=${this.page}&image_type=photo&orientation=horizontal&per_page=40&safesearch=true`);
+        this.incrementPage();
+        return response;
+    }
+
+    incrementPage() {
+        this.page += 1;
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
