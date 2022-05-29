b2ed6133991dffa0ef6f77fc86a404e69cdf7840
Preksler <preksler@gmail.com>
2022-05-29 03:29:13 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -11,12 +11,6 @@ const refs = {
     galleryList: document.querySelector('.gallery'),
 };
 
-const lightbox = new SimpleLightbox('.gallery a', {
-    caption: true,
-    captionsData: 'alt',
-    captionDelay: 250,
-});
-
 const imagesApiService = new ImagesApiService();
 const loadMoreBtn = new LoadMoreBtn({ selector: '.load-more', hidden: true });
 
@@ -32,8 +26,13 @@ function onSearch(e) {
     imagesApiService.fetchImages()
         .then(({ data }) => {
             clearImagesContainer();
-            appendImagesMurkup(data)
-            loadMoreBtn.enable();
+            imagesApiService.setHits(data.totalHits);
+            appendImagesMurkup(data);
+            if(data.totalHits > 0) {
+                loadMoreBtn.enable();
+            } else {
+                loadMoreBtn.hide();
+            }
         })
         .catch(err => {console.log(err);});
 }
@@ -45,24 +44,43 @@ function appendImagesMurkup(data) {
     if (imagesApiService.getPage() === 2 && data.totalHits !== 0) {
         Notify.success(`Hooray! We found ${data.totalHits} images.`);
     }
-    if (data.totalHits !== 0 && data.hits.length === 0) {
+ 
+    if (imagesApiService.totalHits <= 0 && data.totalHits !== 0) {
         loadMoreBtn.hide();
         Notify.failure(`We're sorry, but you've reached the end of search results.`);
-        return;
     }
-    lightbox.refresh();
     refs.galleryList.insertAdjacentHTML('beforeend', imagesMarkup(data));
+    lightBox();
 }
 
 function onLoadMore() {
     loadMoreBtn.disable();
+    imagesApiService.decrementHits();
     imagesApiService.fetchImages()
         .then(({ data }) => {
             appendImagesMurkup(data);
             loadMoreBtn.enable();
+            scrollBottom();
         });
+    lightBox().refresh();
 }
 
 function clearImagesContainer() {
     refs.galleryList.innerHTML = '';
+}
+
+function lightBox() {
+    return new SimpleLightbox('.gallery a', {
+        caption: true,
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
+}
+
+function scrollBottom() {
+      const { height: cardHeight } = refs.galleryList.firstElementChild.getBoundingClientRect();
+        window.scrollBy({
+        top: cardHeight * 2.8,
+        behavior: "smooth",
+        });
 }
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -4,20 +4,10 @@ export default class ImagesApiService {
     constructor() {
         this.searchQuery = '';
         this.page = 1;
+        this.totalHits = null;
     }
 
     async fetchImages() {
-        // const params = {
-        //     key: '27643895-f6cbf0652fd699c6e124c20c0',
-        //     q: this.searchQuery,
-        //     page: this.page,
-        //     image_type: 'photo',
-        //     orientation: 'horizontal',
-        //     per_page: 40,
-        //     safesearch: true,
-        // };
-        // const response = await axios.get(`https://pixabay.com/api/`, params);
-
         const response = await axios.get(`https://pixabay.com/api/?key=27643895-f6cbf0652fd699c6e124c20c0&q=${this.searchQuery}&page=${this.page}&image_type=photo&orientation=horizontal&per_page=40&safesearch=true`);
         this.incrementPage();
         return response;
@@ -33,6 +23,14 @@ export default class ImagesApiService {
 
     getPage() {
         return this.page;
+    }
+
+    setHits(totalHits) {
+        this.totalHits = totalHits - 40;
+    }
+
+    decrementHits() {
+        this.totalHits -= 40;
     }
 
     get query() {
